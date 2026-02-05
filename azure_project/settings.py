1712c3fa3329a52022e2cbfa478dcbca7c3955fa
inned454 <inned454@gmail.com>
2026-02-05 15:05:03 +0700

    Add all files from frontend folder and remove nested git

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -88,18 +88,18 @@ WSGI_APPLICATION = 'azure_project.wsgi.application'
 #     }
 # }
 DATABASES = {
-    'default': {
-        'ENGINE': 'mssql',
-        'NAME': 'bearlab-sql-db',
-        'USER': 'bearlab-admin',
-        'PASSWORD': 'P@ss1234',
-        'HOST': 'bearlab-sql-server.database.windows.net',
-        'PORT': '1433',
-        'OPTIONS': {
-            'driver': 'ODBC Driver 18 for SQL Server',
-        },
-    },
-}
+     'default': {
+         'ENGINE': 'mssql',
+         'NAME': 'free-sql-db-5376513',
+         'USER': 'inn-admin',
+         'PASSWORD': 'Iones454',
+         'HOST': 'inn-sql-server.database.windows.net',
+         'PORT': '1433',
+         'OPTIONS': {
+             'driver': 'ODBC Driver 18 for SQL Server',
+         },
+     },
+ }
 
 
 import urllib.parse
