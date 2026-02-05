2db934316076e821dda78b2bb09241711c3e0fac
inned454 <inned454@gmail.com>
2026-02-05 16:05:58 +0700

    Update settings.py

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -108,7 +108,7 @@ import urllib.parse
 mongo_username = urllib.parse.quote_plus('innadmin')
 mongo_password = urllib.parse.quote_plus('Iones454')
 # MONGO_URI = 'mongodb://localhost:27017/'
-MONGO_URI = f'mongodb+srv://{mongo_username}:{mongo_password}@innadmin:<password>@inn-mongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
+MONGO_URI = f'mongodb+srv://{mongo_username}:{mongo_password}@inn-mongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
 MONGO_DB_NAME = 'inn-mongodb'
 
 # Password validation
